29edc26657db2e4f9e6cab802b5ef67a063e3247
Tomi-PF <tperrino@fi.uba.ar>
2025-10-07 17:57:15 -0300

    Eliminación de código innecesario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import Footer from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
