0b858d91846a6f8e929c5b3aa18330c5012c57f3
Tomi-PF <tperrino@fi.uba.ar>
2025-10-11 00:09:17 -0300

    Creación de la sección de inicio con la utilización de la descripción de la pagina y el carrousel de imágenes de los productos

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+import Description from "../components/Description"
+import Carrousel from "../components/Carrousel"
+
+export default function Inicio(){
+    return(
+        <span>
+            <Description/>
+            <Carrousel/>
+        </span>
+    )
+}
