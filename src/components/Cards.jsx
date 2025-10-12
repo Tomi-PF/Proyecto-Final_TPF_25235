8e942e237411e6e6a850560fdec8c2cefc00b5c0
Tomi-PF <tperrino@fi.uba.ar>
2025-10-11 22:14:14 -0300

    Creación del componente para buscar los productos en función de la categoría elegida

---
+++ src/components/Cards.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import Swal from 'sweetalert2';
+import CardProducto from "../components/CardProducto"
+
+export default function Cards({categoria}){
+    
+    const [productos, setProductos] = useState([])
+
+    const buscarProductos = (categoria) => {
+        fetch(`https://dummyjson.com/products/category/${categoria}`)
+        .then((res) => res.json())
+        .then((prods) => {
+            setProductos(prods.products)
+        })
+        .catch(() => {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Error al cargar los productos"
+            });
+        })
+    }
+
+    useEffect(() => {
+        setProductos([]);
+        if(categoria){
+            buscarProductos(categoria);
+        }
+    }, [categoria]);
+
+    return(
+        <div className="productos-container">
+            {
+                productos.map((p) => (
+                    <CardProducto 
+                        key={p.id}
+                        id={p.id} 
+                        nombre={p.title} 
+                        descripcion={p.description}
+                        foto={p.thumbnail}
+                        precio={p.price}
+                        estrellas={p.rating}
+                    />
+                ))
+            }
+        </div>
+    )
+}
