ab31cc1fbefbc2364c3f7e8871fbc834d26a8240
Tomi-PF <tperrino@fi.uba.ar>
2025-10-11 22:12:36 -0300

    Creación del componente para mostrar los diferentes productos

---
+++ src/components/CardProducto.jsx
@@ -0,0 +1,31 @@
+import { Card, ListGroup } from 'react-bootstrap'
+import Swal from 'sweetalert2'
+import Boton from './Boton'
+
+export default function CardProducto({id, nombre, descripcion, foto, precio, estrellas}){
+
+    const mensaje = () => {
+        Swal.fire({
+            title: "Producto agregado al carrito correctamente",
+            icon: "success",
+            draggable: true
+        });
+    }
+
+    return(
+        <Card key={id} style={{ width: '18rem', marginTop:"10px" }}>
+            <Card.Img variant="top" src={foto} />
+            <Card.Body>
+                <Card.Title>{nombre}</Card.Title>
+                <Card.Text>{descripcion}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroup.Item>Precio: <b>${precio}</b></ListGroup.Item>
+                <ListGroup.Item>Estrellas: <b>{estrellas}</b></ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+                <Boton variante={"primary"} texto={"Agregar al carrito"} funcion={mensaje}/>
+            </Card.Body>
+        </Card>
+    )    
+}
