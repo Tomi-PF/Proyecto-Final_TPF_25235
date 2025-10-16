9b03bf0a783acb88ab04275eb12eec962d196033
Tomi-PF <tperrino@fi.uba.ar>
2025-10-16 11:00:25 -0300

    Modificación cuando se muestra el mensaje por utilización del componente dedicado a realizar esa tarea

--- src/components/CardProducto.jsx
+++ src/components/CardProducto.jsx
@@ -1,15 +1,11 @@
 import { Card, ListGroup } from 'react-bootstrap'
-import Swal from 'sweetalert2'
 import Boton from './Boton'
+import { mostrarAlertaExito } from './Mensajes'
 
 export default function CardProducto({id, nombre, descripcion, foto, precio, estrellas}){
 
     const mensaje = () => {
-        Swal.fire({
-            title: "Producto agregado al carrito correctamente",
-            icon: "success",
-            draggable: true
-        });
+        mostrarAlertaExito("Producto agregado al carrito correctamente")
     }
 
     return(
